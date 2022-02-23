f5b6959fb243eb95d85ba51f66d1a8fb8b90b338
Douglas Joziel Bitencourt Freitas <douglas.joziel@outlook.com>
2022-02-23 19:54:57 -0300

    Fixes file index.js

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -2,30 +2,27 @@
 
 Exercícios de interpretação de código
 
-1. Resposta:
-Inicialmente, o código declara a variável 'valor' e atribui-lhe o valor 0. Em seguida, executa o laço for 5 vezes, somando à variável 'valor' o valor de 'i'. No console.log imprime na tela o valor final igual a 10.
+1. Resposta: 
+O código abaixo soma os valores dos número inteiros do intervalo [0, 5). O resultado exibido na tela é 10.
+   
+2. Resposta:
+a) Serão impressos os números que pertencem ao array 'lista' e são maiores que 18.
 
-2. Respostas:
-a) Serão impressos os valores do array 'lista' maiores que 18, conforme segue: 19, 21, 23, 25, 27, 30.
-
-b) Sim. Com adaptação de um contador é possível ter acesso ao índicie de cada elemento do array 'lista'. O novo código seria assim:
+b) Sim. Dentre as possibilidades, aquela que julgo melhor utilizar é o método indexOf(), da seguite forma:
 
 const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30];
-
-let i = 0;
 for (let numero of lista) {
   if (numero > 18) {
-		console.log(numero)
-   }
-   i++;
+		console.log(lista.indexOf(numero), numero);
+	};
 };
 
 3. Resposta:
-Se o usuário digitasse 4, a saída impressa seria uma sequência de arteriscos da seguinte forma:
-* -> 1ª execução do while
-** -> 2ª execução do while
-*** -> 3ª execução do while
-**** -> 4ª execução do while
+Para o código em questão, a impressão no console para um input igual a 4 é a seguinte:
+*
+**
+***
+****
 
 */
 
@@ -33,120 +30,121 @@ Se o usuário digitasse 4, a saída impressa seria uma sequência de arteriscos 
 
 // 1. Resposta:
 // a), b), c)
-const numeroDePets = +prompt('Quantos pets vocês tem?');
+const numeroDePets = +prompt(`Quantos pets você tem?`);
 let nomesDosPets = [];
 
 if (numeroDePets == 0) {
-   console.log('Que pena! Você pode adotar um pet!');
+   console.log(`Que pena! Você pode adotar um pet!`)
 } else {
    for (let i = 0; i < numeroDePets; i++) {
-      nomesDosPets[i] = prompt(`Qual o nome do seu ${i + 1}º pet?`);
-   };
-   for (let i = 0; i < numeroDePets; i++) {
-      console.log(nomesDosPets[i]);
+      nomesDosPets[i] = prompt(`Nome do ${i+1}º pet:`);
    };
 };
 
-// 2. Resposta:
-// a)
-const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+console.log(nomesDosPets);
 
-for (let k = 0; k < arrayOriginal.length; k++) {
-   console.log(arrayOriginal[k]);
+// 2. Resposta:
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+
+// a)
+for (numero of array) {
+   console.log(numero);
 };
 
 // b)
-for (let k of arrayOriginal) {
-   console.log(k/10);
+for (numero of array) {
+   console.log(numero/10);
 };
 
 // c)
-let arrayNovo = [];
-for (let k of arrayOriginal) {
-   if ((k % 2) == 0) {
-      arrayNovo.push(k);
+let novoArray = [];
+for (numero of array) {
+   if (numero % 2 == 0) {
+      novoArray.push(numero);
    };
 };
 
-console.log(arrayNovo);
+console.log(novoArray);
 
 // d)
-let arrayFrase = [];
-for (let k = 0; k < arrayOriginal.length; k++) {
-   arrayFrase[k] = `O elemento do índice ${k} é igual a ${arrayOriginal[k]}`;
-};
-
-for (let frase of arrayFrase) {
-   console.log(frase);
+let fraseArray = [];
+for (numero of array) {
+   fraseArray.push(`O elemento do índex ${array.indexOf(numero)} é: ${numero}`);
+   console.log(fraseArray[array.indexOf(numero)]);
 };
 
 // e)
-let minimo = arrayOriginal[0]; 
-let maximo = arrayOriginal[0];
-
-for (let numero of arrayOriginal) {
+let minimo = array[0], maximo = minimo;
+for (numero of array) {
    if (numero < minimo) {
       minimo = numero;
-   };
-   if (numero > maximo) {
+   } else if (numero > maximo) {
       maximo = numero;
    };
 };
 
 console.log(`O maior número é ${maximo} e o menor é ${minimo}`);
 
-// Desafio
+// Desafios
 
-// 1.
-// a), b), c)
+// 1. Resposta:
 function jogoOriginal() {
-   const numeroSecreto = +prompt('Digite o número secreto:');
-   console.log('Vamos jogar!');
+   let numero = +prompt(`Digite um número secreto:`);
+   let palpite;
 
-   let chute;
-   let tentativas = 0;
+   console.log(`Vamos jogar um jogo!`);
+
+   tentativas = 0;
    do {
-      chute = +prompt('Qual é o seu chute:');
-      if (chute < numeroSecreto) {
-         console.log(`O número chutado foi: ${chute}\nErrrrrrrrou, é maior`);
-      };
-      if (chute > numeroSecreto) {
-         console.log(`O número chutado foi: ${chute}\nErrrrrrrrou, é menor`);
-      };
-      tentativas++;
-   } while (chute != numeroSecreto);
+      palpite = +prompt(`Adivinhe o número secreto:`);
+      console.log(`Seu chute foi ${palpite}!`);
 
-   console.log(`Acertou!\nO número de tentativas foi: ${tentativas}`);
+      if (palpite < numero) {
+         console.log(`Errrrrrrrou, o número secreto é maior!`);
+      } else if (palpite > numero) {
+         console.log(`Errrrrrrrou, o número secreto é menor!`);
+      };
+
+      tentativas++;
+   } while (palpite != numero);
+
+   console.log(`Acertou!!!`);
+   console.log(`Tentativas: ${tentativas}`);
 };
 
 jogoOriginal();
 
-// 2. 
+// 2. Resposta:
 function jogoModificado() {
-   const numeroSecreto = Math.floor(Math.random() * 100) + 1;
-   // console.log(numeroSecreto);
-   console.log('Vamos jogar!');
+   let numero = Math.floor(Math.random() * 99) + 1;
+   let palpite;
 
-   let chute;
-   let tentativas = 0;
+   // console.log(numero);
+   console.log(`Vamos jogar um jogo!`);
+
+   tentativas = 0;
    do {
-      chute = +prompt('Qual é o seu chute (entre 1 e 100):');
-      if (chute < numeroSecreto) {
-         console.log(`O número chutado foi: ${chute}\nErrrrrrrrou, é maior`);
-      };
-      if (chute > numeroSecreto) {
-         console.log(`O número chutado foi: ${chute}\nErrrrrrrrou, é menor`);
-      };
-      tentativas++;
-   } while (chute != numeroSecreto);
+      palpite = +prompt(`Adivinhe o número secreto:`);
+      
+      console.log(`Seu chute foi ${palpite}!`);
 
-   console.log(`Acertou!\nO número de tentativas foi: ${tentativas}`);
+      if (palpite < numero) {
+         console.log(`Errrrrrrrou, o número secreto é maior!`);
+      } else if (palpite > numero) {
+         console.log(`Errrrrrrrou, o número secreto é menor!`);
+      };
+
+      tentativas++;
+   } while (palpite != numero);
+
+   console.log(`Acertou!!!`);
+   console.log(`Tentativas: ${tentativas}`);
 };
 
 jogoModificado();
 
 /*
 
-A alteração foi extremamente simples. Bastou remover o input do outro jogar e solicitar que o computador gerasse um número aleatório. Existem muitos algoritmos para, a partir de uma semente, gerar números aleatórios e/ou pseudo aleatórios; entretanto, o método mais prático em JS é utilizar as propriedades floor() e random() do objeto Math.
+A alteração foi extremamente simples. Bastou remover o input via prompt, feito pelo 1º jogador, e solicitar que o computador gerasse um número aleatório. Existem muitos algoritmos para gerar números pseudo-aleatórios; entretanto, o método mais prático em JS é utilizar as propriedades floor() e random() do objeto Math.
 
 */
