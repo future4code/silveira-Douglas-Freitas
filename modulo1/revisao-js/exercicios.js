61907a31661428a9bbd35986eed88ae86cf8d384
Douglas Joziel Bitencourt Freitas <douglas.joziel@outlook.com>
2022-03-07 10:34:28 -0300

    Exercise 9 completed

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -79,8 +79,14 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
-}
+   if (ladoA == ladoB && ladoB == ladoC && ladoC == ladoA) {
+      return 'Equilátero';
+   } else if (ladoA != ladoB && ladoB != ladoC && ladoC != ladoA) {
+      return 'Escaleno';
+   } else {
+      return 'Isósceles';
+   };
+};
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
